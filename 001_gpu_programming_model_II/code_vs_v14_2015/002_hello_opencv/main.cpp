97e529f14f6c89f605b079455113a06b2fe33695
romualdo97 <romualdorojo97@gmail.com>
2017-12-22 12:06:03 -0500

    Solving windows plugin error when invoking namedWindow method

--- 001_gpu_programming_model_II/code_vs_v14_2015/002_hello_opencv/main.cpp
+++ 001_gpu_programming_model_II/code_vs_v14_2015/002_hello_opencv/main.cpp
@@ -5,24 +5,35 @@
 
 using namespace cv;
 using namespace std;
-
+// unlinked opencv_calib3d331d.lib
+// unlinked opencv_features2d331d.lib
 int main(int argc, char** argv)
 {
-	if (argc != 2)
+	/*if (argc != 2)
 	{
 		cout << "Usage 002_hello_opencv.exe [image_to_load_and_display]\n";
 		return -1;
-	}
-	cout << argv[1] << endl;
+	}*/
+	//cout << argv[1] << endl;
 	Mat image;
-	image = imread(argv[1], IMREAD_COLOR); // load the image
-	if (image.empty()) // check for file errors
+	image = imread("1.bmp", IMREAD_COLOR); // load the image
+	if (NULL == image.data) // check for file errors
 	{
+		/*
+		- why image is not loading???????????
+		I change the format to window bitmap *.bmp
+		which opencv always support with no needs of additional
+		libraries.
+		- How make imgread to read traditional formatas?????
+		Read here https://docs.opencv.org/3.3.0/d4/da8/group__imgcodecs.html#ga288b8b3da0892bd651fce07b3bbd3a56
+		about libjpg, libpng, libtiff, libjasper for know about read other more
+		traditional formats. (see the notes section of imread method)
+		*/
 		cout << "Could not open or find the file\n";
 		return -1;
 	}
 	namedWindow("display window", CV_WINDOW_AUTOSIZE); // Create a window for display.
-	imshow("display window", image); // Show our image inside it.
-	waitKey(0); // Wait for a keystroke in the window
+	//imshow("display window", image); // Show our image inside it.
+	//waitKey(0); // Wait for a keystroke in the window
 	return 0;
 }
